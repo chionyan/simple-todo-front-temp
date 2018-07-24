46235648313e4472d48201a5b225fd4d4e9aa0f3
chionyan <ariariarissa@gmail.com>
2018-07-24 16:55:11 +0900

    カードの作成

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ class App extends React.Component {
     return (
       <div className="app">
         <h3>Todo List</h3>
-        <TodoItem />
+        <TodoItem
+          title="Todo1のタイトル"
+          text="Todo1のテキスト"
+          created_at="Todo1の作成日時"
+        />
+        <TodoItem
+          title="Todo2のタイトル"
+          text="Todo2のテキスト"
+          created_at="Todo2の作成日時"
+        />
       </div>
     );
   }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 class TodoItem extends React.Component {
   render() {
+    const title = this.props.title;
+    const text = this.props.text;
+    const created_at = this.props.created_at;
     return (
       <tr>
-        <td>Todo1のタイトル</td>
-        <td>Todo1のテキスト</td>
-        <td>Todo1の作成日時</td>
+        <td>{title}</td>
+        <td>{text}</td>
+        <td>{created_at}</td>
       </tr>
     );
   }
